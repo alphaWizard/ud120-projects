82f297740bef7a1fc838cd0921fc7c9db200fbff
alphawizard <beitmyway@gmail.com>
2018-08-15 23:41:17 +0530

    decision tree part done

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -20,10 +20,25 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
+print "no. of features: ", len(features_train[0])
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1=time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+accuracy = accuracy_score(labels_test,pred)
+print "accuracy: ", (accuracy)
 
 
 #########################################################
