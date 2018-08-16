1a41cf09b1944e63f8fcd3b810c7cd1f451e63ab
alphawizard <beitmyway@gmail.com>
2018-08-16 15:22:16 +0530

    choose your own part

--- choose_your_own/class_vis.py
+++ choose_your_own/class_vis.py
@@ -7,6 +7,7 @@ import pylab as pl
 def prettyPicture(clf, X_test, y_test):
     x_min = 0.0; x_max = 1.0
     y_min = 0.0; y_max = 1.0
+
     
     # Plot the decision boundary. For that, we will assign a color to each
     # point in the mesh [x_min, m_max]x[y_min, y_max].
--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -16,15 +16,18 @@ grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if l
 bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
 
 
-#### initial visualization
-plt.xlim(0.0, 1.0)
-plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
-plt.legend()
-plt.xlabel("bumpiness")
-plt.ylabel("grade")
-plt.show()
+#### for initial visualization
+
+
+# plt.xlim(0.0, 1.0)
+# plt.ylim(0.0, 1.0)
+# plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+# plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+# plt.legend()
+# plt.xlabel("bumpiness")
+# plt.ylabel("grade")
+# plt.show()
+
 ################################################################################
 
 
@@ -32,6 +35,65 @@ plt.show()
 ### visualization code (prettyPicture) to show you the decision boundary
 
 
+
+from time import time
+
+
+## 1. naive bayes --- > 88.4%
+
+# from sklearn.naive_bayes import GaussianNB as gnb
+# from sklearn.metrics import accuracy_score
+# clf = gnb()
+
+# t0 = time()
+# clf.fit(features_train,labels_train)
+# print "training time:", round(time()-t0, 3), "s"
+
+# t1 = time()
+# prediction = clf.predict(features_test)
+# print "prediction time:", round(time()-t1, 3), "s"
+
+# accuracy = accuracy_score(labels_test,prediction)
+# print "accuracy: ", (accuracy)
+
+
+
+## 2. svm ----> 94.4%
+
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+clf = SVC(C=100000, kernel="rbf")
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1=time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+accuracy = accuracy_score(labels_test,pred)
+print "accuracy: ", (accuracy)
+
+
+## 3. decision tree ---> 92.4 
+
+# from sklearn import tree
+# from sklearn.metrics import accuracy_score
+
+# clf = tree.DecisionTreeClassifier(min_samples_split=20)
+
+# t0 = time()
+# clf.fit(features_train, labels_train)
+# print "training time:", round(time()-t0, 3), "s"
+
+# t1=time()
+# pred = clf.predict(features_test)
+# print "prediction time:", round(time()-t1, 3), "s"
+
+# accuracy = accuracy_score(labels_test,pred)
+# print "accuracy: ", (accuracy)
 
 
 
@@ -41,4 +103,5 @@ plt.show()
 try:
     prettyPicture(clf, features_test, labels_test)
 except NameError:
-    pass
+	pass
+    
